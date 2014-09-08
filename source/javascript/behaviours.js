b1bc8ee7ad6f89e9b69610c5f19148f425838e68
sunaiwen <sunivengg@gmail.com>
2014-09-08 16:35:23 +0800

    fix sidebar transtion javascript control bug

--- source/javascript/behaviours.js
+++ source/javascript/behaviours.js
@@ -1,12 +1,23 @@
 (function(doc){
     var sideBarCover = document.getElementsByClassName('js-sideBarCover')[0];
     var menuBtn = document.getElementsByClassName('js-showSideBar')[0];
+    var sidebar = document.getElementsByClassName('js-sidebar')[0];
+    var actionClass = 'is-showSideBar';
+    var transitionHandler = function(){
+        sidebar.style.zIndex = 1;
+        sideBarCover.removeEventListener('transitionend', transitionHandler);
+        sideBarCover.removeEventListener('webkitTransitionend', transitionHandler);
+    };
     menuBtn.addEventListener('click', function(e){
         e.preventDefault();
         var coverClassList = sideBarCover.classList;
-        console.log(coverClassList);
-        coverClassList.contains('is-showSideBar') ?
-            coverClassList.remove('is-showSideBar'):
+        if(coverClassList.contains(actionClass)) {
+            coverClassList.remove('is-showSideBar');
+            sidebar.style.zIndex = -1;
+        } else {
             coverClassList.add('is-showSideBar');
+            sideBarCover.addEventListener('transitionend', transitionHandler);
+            sideBarCover.addEventListener('webkitTransitionend', transitionHandler);
+        }
     });
 })(document);
